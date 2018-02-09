fdf0094464f2a70f5659997f2c016371882ee517
Justus Fowl <justusfowl@gmail.com>
2018-02-09 09:57:56 +0100

    modify backup script

--- backup/createBackupJob.ps1
+++ backup/createBackupJob.ps1
@@ -1,3 +1,5 @@
+# @Author: JustusFowl
+
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -File "C:\scripts\backupVMs.ps1" -flagCoreVMs' 
 $trigger =  New-ScheduledTaskTrigger -Daily -At 23pm
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "BackupVMs" -Description "Daily run of the backup script"
