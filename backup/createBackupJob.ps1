8afddc38c3541c0860ba81106bc6331df0f452fe
Justus Fowl <justusfowl@gmail.com>
2018-03-10 12:58:21 +0100

    logfile sched-job

--- backup/createBackupJob.ps1
+++ backup/createBackupJob.ps1
@@ -1,5 +1,5 @@
 # @Author: JustusFowl
 
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -File "C:\scripts\powershell\backup\backupVMs.ps1" -flagCoreVMs *> ..\..\logs\"BackupVM-$(get-date -f yyyy-MM-dd).log"'
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -File "C:\scripts\powershell\backup\backupVMs.ps1" -flagCoreVMs *> "..\..\logs\BackupVM-$(get-date -f yyyy-MM-dd).log"'
 $trigger =  New-ScheduledTaskTrigger -Daily -At 23pm
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "BackupVMs" -Description "Daily run of the backup script"
