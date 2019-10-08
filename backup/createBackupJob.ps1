5d2607b8f4096bba855a147de077107abef498dd
Administrator@PRAXIS.local <Administrator@PRAXIS.local>
2019-10-08 09:46:17 +0200

    modify create-job ps

--- backup/createBackupJob.ps1
+++ backup/createBackupJob.ps1
@@ -1,5 +1,11 @@
 # @Author: JustusFowl
 
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -File "C:\scripts\powershell\backup\backupVMs.ps1" -flagCoreVMs'
+$configFilePath = "C:\scripts\powershell\backup\backupVMs.config.ps1"
+
+$arg = -join('-NoProfile -WindowStyle Hidden -File "C:\scripts\powershell\backup\backupVMs.ps1" -flagCoreVMs -PathConfigFile "', $configFilePath, '"')
+
+echo "The Job BackupVMs shall be created with $arg"
+
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument $arg
 $trigger =  New-ScheduledTaskTrigger -Daily -At 23pm
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "BackupVMs" -Description "Daily run of the backup script"
